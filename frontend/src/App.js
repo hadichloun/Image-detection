437e05b7eac6347474e05b85c65f92fd62bfd4c6
hadi chloun <hadi_960@hotmail.com>
2022-08-08 15:40:45 -0500

    adds thead tag and set states to handle the inagePredict and outputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Stack } from "react-bootstrap";
 import InputImage from "./InputImage";
+import Output from "./Output";
 
 function App() {
+  const [outputs, setOutputs] = useState([]);
+  const [imageToPredict, setImageToPredict] = useState("");
+
   return (
     <Container >
       <div className="mt-3" />
+      <Stack gap={2}>
      <InputImage />
+     <Output outputs={outputs} imageToPredict={imageToPredict} />
+     </Stack>
     </Container>
   );
 }
